e9053b354ac5f9c6d494e074a6b0c80c9b67a3bc
Lilac <ileyd@icloud.com>
2018-02-27 22:15:23 +1000

    update imports

--- __init__.py
+++ __init__.py
@@ -6,6 +6,6 @@ from .catalogue import Catalogue
 from .course import Course, CourseSemestersOffered
 from .plan import Plan
 from .program import Program
-from .semester import Semester, get_all_semesters
+from .semester import Semester, get_all_semesters, get_semester_by_name
 from .assessment import Assessment
 from .offering import Offering
